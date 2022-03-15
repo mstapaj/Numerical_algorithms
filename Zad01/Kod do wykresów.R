cb67c74c6d7d5fee40a4026a4981a69085681206
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-03-15 22:04:33 +0100

    Zmiany w kodzie do wykresów

--- Zad01/Kod do wykresów.R
+++ Zad01/Kod do wykresów.R
@@ -1,52 +1,61 @@
+# dla windowsa
 data <- read.csv("D:/Projekty/Projekty Java/Algorytmy numeryczne/Algorytmy_numeryczne/Zad01/test.csv")
+# dla linuxa
+data <- read.csv("/home/mtx22/Pulpit/Algorytmy numeryczne/Algorytmy_numeryczne/Zad01/test.csv")
+
 View(data)
 class(data)
 
-par(mfrow=c(1,1))
-hist(data$Taylor_od_lewej,prob=T)
 
-# sprawdzenie
-plot(data$Kat,data$Taylor_od_lewej,type="l",xlab="k¹t",ylab="wartoœæ")
-plot(data$Kat,data$Taylor_od_prawej,type="l",xlab="k¹t",ylab="wartoœæ")
-plot(data$Kat,data$Taylor_od_lewej_prev,type="l",xlab="k¹t",ylab="wartoœæ")
-plot(data$Kat,data$Taylor_od_prawej_prev,type="l",xlab="k¹t",ylab="wartoœæ")
-plot(data$Kat,data$Taylor_z_func_wbudowanych,type="l",xlab="k¹t",ylab="wartoœæ")
-
-# funkcja na indeks log
-function(x){
+# funkcja dzielÄ…ca dane na zbiory po 10000 elementÃ³w i wyliczajaca z nich srednia
+prepareAvg<-function(x){
   res<-c()
-  for (i in seq(1:length((x)))){
-    if (sqrt(i)%%1==0) {
-      c<-append(res,i)
-    }
+  for (i in 1:100){
+    a<-10000*(i-1)
+    b<-10000*i
+    res<-append(res,mean(x[a:b]))
   }
-  return (res);
+  return(res)
 }
 
-
-# ³¹czenie wykresów
-plot(data$Kat,data$Taylor_od_lewej,type="l",xlab="k¹t",ylab="wartoœæ")
-lines(data$Kat,data$Taylor_od_prawej,col="RED")
+plot(prepareAvg(test_data), type = "l")
+plot(prepareAvg(data$Kat))
+plot(prepareAvg(data$Taylor_od_lewej), type='l')
+plot(data$Taylor_od_lewej)
 
 
 # Oddzielne wykresy 
 par(mfrow=c(1,1))
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",type="l",col="RED",main = "Ró¿nica miêdzy sumowaniem elementów szeregu Taylora od pocz¹tku, a wbudowan¹ funkcj¹ biblioteczn¹ ")
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",col="RED",type = "l",main = "Ró¿nica miêdzy sumowaniem elementów szeregu Taylora od koñca, a wbudowan¹ funkcj¹ biblioteczn¹ ")
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej_prev)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",col="RED",type = "l",main = "Ró¿nica miêdzy sumowaniem elementów szeregu Taylora od pocz¹tku, wyliczaj¹c na podstawie poprzedniego wyrazu, a wbudowan¹ funkcj¹ biblioteczn¹ ")
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej_prev)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",col="RED",type = "l",main = "Ró¿nica miêdzy sumowaniem elementów szeregu Taylora od koñca, wyliczaj¹c na podstawie poprzedniego wyrazu, a wbudowan¹ funkcj¹ biblioteczn¹ ")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych)),xlab = "Warto?ci argument?w", ylab="Warto?ci b?edu wzgl?dnego",type="l",col="RED",main = "R??nica mi?dzy sumowaniem element?w szeregu Taylora od pocz?tku, a wbudowan? funkcj? biblioteczn? ")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych)),xlab = "Warto?ci argument?w", ylab="Warto?ci b?edu wzgl?dnego",col="RED",type = "l",main = "R??nica mi?dzy sumowaniem element?w szeregu Taylora od ko?ca, a wbudowan? funkcj? biblioteczn? ")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "Warto?ci argument?w", ylab="Warto?ci b?edu wzgl?dnego",col="RED",type = "l",main = "R??nica mi?dzy sumowaniem element?w szeregu Taylora od pocz?tku, wyliczaj?c na podstawie poprzedniego wyrazu, a wbudowan? funkcj? biblioteczn? ")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "Warto?ci argument?w", ylab="Warto?ci b?edu wzgl?dnego",col="RED",type = "l",main = "R??nica mi?dzy sumowaniem element?w szeregu Taylora od ko?ca, wyliczaj?c na podstawie poprzedniego wyrazu, a wbudowan? funkcj? biblioteczn? ")
 
 
-# Jeden duzy wykres, polaczany
+# Jeden duzy wykres, polaczany, taka sama os Y
 par(mfrow=c(2,2))
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",type="l",col="RED",main = "Sumowanie elementów szeregu Taylora od pocz¹tku")
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",col="RED",type = "l",main = "Sumowanie elementów szeregu Taylora od koñca")
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej_prev)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",col="RED",type = "l",main = "Sumowanie elementów szeregu Taylora od pocz¹tku, wyliczaj¹c na podstawie poprzedniego wyrazu")
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej_prev)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",col="RED",type = "l",main = "Sumowanie elementów szeregu Taylora od koñca, wyliczaj¹c na podstawie poprzedniego wyrazu")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",type="l",col="RED",main = "Sumowanie elementÃ³w od poczÄ…tku",ylim = c(0,0.00000015) )
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",col="RED",type = "l",main = "Sumowanie elementÃ³w od koÅ„ca",ylim = c(0,0.00000015))
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",col="RED",type = "l",main = "Sumowanie elementÃ³w od poczÄ…tku, wyliczajÄ…c na podstawie poprzedniego wyrazu",ylim = c(0,0.00000015))
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",col="RED",type = "l",main = "Sumowanie elementÃ³w od koÅ„ca, wyliczajÄ…c na podstawie poprzedniego wyrazu",ylim = c(0,0.00000015))
 
+# Jeden duzy wykres, polaczany, roÅ¼ne wartoÅ›ci na osi Y
+par(mfrow=c(2,2))
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",type="l",col="RED",main = "Sumowanie elementÃ³w od poczÄ…tku")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",col="RED",type = "l",main = "Sumowanie elementÃ³w od koÅ„ca")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",col="RED",type = "l",main = "Sumowanie elementÃ³w od poczÄ…tku, wyliczajÄ…c na podstawie poprzedniego wyrazu")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",col="RED",type = "l",main = "Sumowanie elementÃ³w od koÅ„ca, wyliczajÄ…c na podstawie poprzedniego wyrazu")
 
 # H1
-plot(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych),xlab = "Wartoœci argumentów", ylab="Wartoœci b³edu wzglêdnego",type="l",main = "Ró¿nica miêdzy sumowaniem elementów szeregu Taylora od pocz¹tku, a sumowaniem od koñca ",col="BLUE")
-lines(data$Kat,abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych),col="RED",type = "l")
-legend(x="topleft", legend=c("Sumowanie od pocz¹tku", "Sumowanie od koñca"),
+par(mfrow=c(1,1))
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",type="l",main = "RÃ³Å¼nica miÄ™dzy sumowaniem elementÃ³w szeregu Taylora od poczÄ…tku, a sumowaniem od koÅ„ca ",col="BLUE")
+lines(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych)),col="RED",type = "l")
+legend(x="topleft", legend=c("Sumowanie od poczÄ…tku", "Sumowanie od koÅ„ca"),
        col=c("blue", "red"), lty=c(1,1),lwd = 2)
+
+# H3
+par(mfrow=c(1,2))
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej)/data$Taylor_z_func_wbudowanych)-abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_lewej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",type="l",col="RED",main = "Sumowanie elementÃ³w od poczÄ…tku")
+plot(prepareAvg(data$Kat),prepareAvg(abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej)/data$Taylor_z_func_wbudowanych)-abs((data$Taylor_z_func_wbudowanych-data$Taylor_od_prawej_prev)/data$Taylor_z_func_wbudowanych)),xlab = "WartoÅ›ci argumentÃ³w", ylab="WartoÅ›ci bÅ‚edu wzglÄ™dnego",col="RED",type = "l",main = "Sumowanie elementÃ³w od koÅ„ca")
+
+
