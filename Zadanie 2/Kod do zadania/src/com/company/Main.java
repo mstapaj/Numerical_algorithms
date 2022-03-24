0ca3c7e1e61db61443ef4d997e9190d4b3f1b4ce
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-03-24 19:45:02 +0100

    Próby implementacji algorytmu Gaussa

--- Zadanie 2/Kod do zadania/src/com/company/Main.java
+++ Zadanie 2/Kod do zadania/src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,42 +19,72 @@ public class Main {
             return matrix;
         }
 
-        public List<List<Float>> adding(MojaMacierz another_matrix){
+        public List<List<Float>> adding(MojaMacierz another_matrix) {
             List<List<Float>> new_matrix = new ArrayList<>();
-            if (this.matrix.size()==another_matrix.getMatrix().size()){
-                for (int i=0;i<this.matrix.size();i++){
+            if (this.matrix.size() == another_matrix.getMatrix().size()) {
+                for (int i = 0; i < this.matrix.size(); i++) {
                     List<Float> row = new ArrayList<>();
-                    for (int j=0;j<another_matrix.getMatrix().get(i).size();j++){
-                        row.add(this.matrix.get(i).get(j)+another_matrix.getMatrix().get(i).get(j));
+                    for (int j = 0; j < another_matrix.getMatrix().get(i).size(); j++) {
+                        row.add(this.matrix.get(i).get(j) + another_matrix.getMatrix().get(i).get(j));
                     }
                     new_matrix.add(row);
                 }
             }
             return new_matrix;
         }
-        public List<List<Float>> multiply(MojaMacierz another_matrix){
+
+        public List<List<Float>> multiply(MojaMacierz another_matrix) {
             List<List<Float>> new_matrix = new ArrayList<>();
-//            if (this.matrix.size()==another_matrix.getMatrix().size()){
-                for (int i=0;i<this.matrix.size();i++){
-                    List<Float> row = new ArrayList<>();
-                    for (int j=0;j<another_matrix.getMatrix().get(i).size();j++){
-                        float summary= 0F;
-                        for (int k=0;k<another_matrix.getMatrix().size();k++){
-                            summary+=this.matrix.get(i).get(k)*another_matrix.getMatrix().get(k).get(j);
-                        }
-                        row.add(summary);
+            for (int i = 0; i < this.matrix.size(); i++) {
+                List<Float> row = new ArrayList<>();
+                for (int j = 0; j < another_matrix.getMatrix().get(i).size(); j++) {
+                    float summary = 0F;
+                    for (int k = 0; k < another_matrix.getMatrix().size(); k++) {
+                        summary += this.matrix.get(i).get(k) * another_matrix.getMatrix().get(k).get(j);
                     }
-                    new_matrix.add(row);
+                    row.add(summary);
                 }
-//            }
+                new_matrix.add(row);
+            }
             return new_matrix;
         }
+
+//        public List<List<Float>> gauss() {
+//            float[][] macWyn = new float [this.matrix.size()][this.matrix.size()];
+//            float[] wynik = {0, 0, 0};
+//            float[] wCopy = new float [wynik.length];
+//            System.arraycopy(wynik, 0, wCopy, 0, wynik.length);
+//            for (int i = 0; i < this.matrix.size()-1; i++) {
+//                for (int j = i + 1; j <= this.matrix.size() - 1; j++) {
+//                    for (int k = 0; k < this.matrix.size(); k++) {
+//                        macWyn[j][k] = this.matrix.get(j).get(k) - (this.matrix.get(i).get(k) * (this.matrix.get(j).get(i) / this.matrix.get(i).get(i)));
+//                    }
+//
+//                    wCopy[j] = wynik[j] - (wynik[i] * (this.matrix.get(j).get(i) / this.matrix.get(i).get(i)));
+//
+//                    for (int ii = 0; ii < macWyn.length; ii++) {
+//                        for (int jj = 0; jj < macWyn.length; jj++) {
+//                            this.matrix.get(ii).set(jj, macWyn[ii][jj]);
+//                        }
+//                        wynik[ii] = wCopy[ii];
+//                    }
+//                }
+//            }
+//            for (int j = 0; j < this.matrix.size(); j++) {
+//                for (int k = 0; k < this.matrix.size(); k++) {
+//                    System.out.print(this.matrix.get(j).get(k) + "\t");
+//                }
+//                System.out.println("\t" + wynik[j]);
+//            }
+//            return this.matrix;
+//        }
     }
 
     public static void main(String[] args) {
         MojaMacierz temp = new MojaMacierz();
         MojaMacierz temp2 = new MojaMacierz();
         MojaMacierz temp3 = new MojaMacierz();
+        MojaMacierz temp4 = new MojaMacierz();
         List<Float> row1 = new ArrayList<>();
         List<Float> row2 = new ArrayList<>();
         List<Float> row3 = new ArrayList<>();
@@ -63,26 +94,36 @@ public class Main {
         List<Float> row7 = new ArrayList<>();
         List<Float> row8 = new ArrayList<>();
         List<Float> row9 = new ArrayList<>();
-        Collections.addAll(row1,-1F,-2F,3F);
-        Collections.addAll(row2,0F,2F,-1F);
-        Collections.addAll(row3,-1F,3F,0F);
-        Collections.addAll(row4,1F,5F,1F);
-        Collections.addAll(row5,2F,1F,2F);
-        Collections.addAll(row6,3F,2F,3F);
-        Collections.addAll(row7,1F);
-        Collections.addAll(row8,2F);
-        Collections.addAll(row9,3F);
+        List<Float> row10 = new ArrayList<>();
+        List<Float> row11 = new ArrayList<>();
+        List<Float> row12 = new ArrayList<>();
+        Collections.addAll(row1, -1F, -2F, 3F);
+        Collections.addAll(row2, 0F, 2F, -1F);
+        Collections.addAll(row3, -1F, 3F, 0F);
+        Collections.addAll(row4, 1F, 5F, 1F);
+        Collections.addAll(row5, 2F, 1F, 2F);
+        Collections.addAll(row6, 3F, 2F, 3F);
+        Collections.addAll(row7, 1F);
+        Collections.addAll(row8, 2F);
+        Collections.addAll(row9, 3F);
+        Collections.addAll(row10, 1F,-2F,3F,-7F);
+        Collections.addAll(row11, 3F,1F,4F,5F);
+        Collections.addAll(row12, 2F,5F,1F,18F);
         List<List<Float>> rows = new ArrayList<>();
         List<List<Float>> rows2 = new ArrayList<>();
         List<List<Float>> rows3 = new ArrayList<>();
-        Collections.addAll(rows,row1,row2,row3);
+        List<List<Float>> rows4 = new ArrayList<>();
+        Collections.addAll(rows, row1, row2, row3);
         temp.setMatrix(rows);
-        Collections.addAll(rows2,row4,row5,row6);
+        Collections.addAll(rows2, row4, row5, row6);
         temp2.setMatrix(rows2);
-        Collections.addAll(rows3,row7,row8,row9);
+        Collections.addAll(rows3, row7, row8, row9);
         temp3.setMatrix(rows3);
+        Collections.addAll(rows4, row10, row11, row12);
+        temp4.setMatrix(rows4);
         System.out.println(temp.adding(temp2));
         System.out.println(temp.multiply(temp2));
         System.out.println(temp.multiply(temp3));
+//        System.out.println(temp4.gauss());
     }
 }
