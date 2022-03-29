10764f407e6e94cb86d00a31f0ae2206f9006bec
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-03-29 19:44:20 +0200

    Dodanie pliku .gitignore, dodanie funkcji generującej macierze różnego typu.

--- Zadanie 2/Kod do zadania/src/com/company/GenerateMatrix.java
+++ Zadanie 2/Kod do zadania/src/com/company/GenerateMatrix.java
@@ -1,62 +1,40 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class GenerateMatrix {
 
-    public static List<List<Fraction>> generate(int cols, int rows) {
-        List<List<Fraction>> result = new ArrayList<>();
-        for (int i = 0; i < rows; i++) {
-            List<Fraction> row = new ArrayList<>();
-            for (int j = 0; j < cols; j++) {
-                Random ran = new Random();
-                row.add(new Fraction(BigInteger.valueOf(ran.nextInt()), BigInteger.valueOf(ran.nextInt())));
+    public static void generate(int cols, int rows) {
+        try {
+            FileWriter myWriterFraction = new FileWriter("fractionMatrix.txt");
+            FileWriter myWriterDouble = new FileWriter("doubleMatrix.txt");
+            FileWriter myWriterFloat = new FileWriter("floatMatrix.txt");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    Random ran = new Random();
+                    BigInteger num = BigInteger.valueOf(ran.nextInt());
+                    BigInteger denum = BigInteger.valueOf(ran.nextInt());
+                    myWriterFloat.write((int) num.floatValue() / denum.floatValue() + ",");
+                    myWriterDouble.write(num.doubleValue() / denum.doubleValue() + ",");
+                    myWriterFraction.write(num + ";" + denum + ",");
+                }
+                myWriterFloat.write("\n");
+                myWriterDouble.write("\n");
+                myWriterFraction.write("\n");
             }
-            result.add(row);
+            myWriterFloat.close();
+            myWriterDouble.close();
+            myWriterFraction.close();
+        } catch (IOException err) {
+            err.printStackTrace();
         }
-        return result;
     }
 
-    public static List<List<Float>> makeFloatMatrix(List<List<Fraction>> matrixFraction) {
-        List<List<Float>> result = new ArrayList<>();
-        for (int i = 0; i < matrixFraction.size(); i++) {
-            List<Float> row = new ArrayList<>();
-            for (int j = 0; j < matrixFraction.get(i).size(); j++) {
-                row.add(matrixFraction.get(i).get(j).getNumerator().floatValue() / matrixFraction.get(i).get(j).getDenumerator().floatValue());
-            }
-            result.add(row);
-        }
-        return result;
-    }
-
-    public static List<List<Double>> makeDoubleMatrix(List<List<Fraction>> matrixFraction) {
-        List<List<Double>> result = new ArrayList<>();
-        for (int i = 0; i < matrixFraction.size(); i++) {
-            List<Double> row = new ArrayList<>();
-            for (int j = 0; j < matrixFraction.get(i).size(); j++) {
-                row.add(matrixFraction.get(i).get(j).getNumerator().doubleValue() / matrixFraction.get(i).get(j).getDenumerator().doubleValue());
-            }
-            result.add(row);
-        }
-        return result;
-    }
-
-//    public static List<List<Double>> saveMatrixToFile(List<List<Fraction>> matrixFraction) {
-//        List<List<Double>> result = new ArrayList<>();
-//        for (int i = 0; i < matrixFraction.size(); i++) {
-//            List<Double> row = new ArrayList<>();
-//            for (int j = 0; j < matrixFraction.get(i).size(); j++) {
-//                row.add(matrixFraction.get(i).get(j).getNumerator().doubleValue()/matrixFraction.get(i).get(j).getDenumerator().doubleValue());
-//            }
-//            result.add(row);
-//        }
-//        return result;
-//    }
 
     public static void main(String[] args) {
-
+        generate(5, 5);
     }
 }
